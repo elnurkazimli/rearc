e8b00e245ad352249d2da76d2d1a68bccc883168
tuncaytas <tnc.tas@gmail.com>
2022-06-19 22:55:58 +0000

    access logs removed

--- alb.tf
+++ alb.tf
@@ -1,19 +1,9 @@
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
+resource "aws_lb" "rearc" {
+  name               = "rearc-lb-tf"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
   subnets            = [for subnet in aws_subnet.public : subnet.id]
 
   enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "test-lb"
-    enabled = true
-  }
-
-  tags = {
-    Environment = "production"
-  }
 }
